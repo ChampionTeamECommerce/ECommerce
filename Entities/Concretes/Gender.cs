6bc369b4fd21e0b0aa33ec789fce824e274fdfa9
fdemirag <f2000fatih@hotmail.com>
2024-03-31 17:02:59 +0300

    Gender entity added

---
+++ Entities/Concretes/Gender.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Gender:Entity<Guid>
+    {
+        public string Name { get; set; }
+
+        public ICollection<Clothes> Clothes { get; set; } // iki cinsinde kullandığı kıyafetler unisex olarak tutulacak
+    }
+}
