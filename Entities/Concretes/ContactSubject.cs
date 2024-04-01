b7f67693cb7a7a086b478a324f5cb51c40ebc58f
fdemirag <f2000fatih@hotmail.com>
2024-04-01 16:48:55 +0300

    ContactUs and ContactSubject entities added

---
+++ Entities/Concretes/ContactSubject.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class ContactSubject:Entity<Guid>
+    {
+        public string Name { get; set; }     
+
+
+        public ICollection <ContactUs> ContactUs { get; set; }
+    }
+}
---
+++ Entities/Concretes/ContactUs.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class ContactUs:Entity<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email {  get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+        public string MembershipCard { get; set; }
+
+
+        public Guid ContactSubjectId { get; set; }
+
+        public virtual ContactSubject ContactSubject { get; set; }
+
+    }
+}
